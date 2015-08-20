88a4c7f2aebd0023fa0175b9c7af408bb54756e7
kathrynemary <kathrynemary@gmail.com>
2015-08-19 23:07:03 -0500

    git commit -m "Can select char, broke other thing"

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,35 @@
+#tests and shit!
+
 class Game
   def initialize
     @board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-    @com = "X"
-    @hum = "O"
+    @computer = ""
+    @human = ""
   end
 
-  def start_game
+#move me!
+  def get_symbols(player)
+    puts "What kind of symbol would you like to use for #{player}?"#well this isn't displaying
+    player = gets.chomp
+    puts "'#{player}' you say?"
+    return player
+  end
+
+  def start_game #yeesh this is a lot in one method
     puts "Welcome to my Tic Tac Toe game"
+    computer = get_symbols(computer)
+    human = get_symbols(human)
+    determine_order
+    
+  end
+
+  def human_first  #well this is not working.
     puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|\n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|\n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|\n"
-    puts "Please select your spot."
+    puts "Please select your spot." 
+    play_game
+  end
+    
+  def play_game
     until game_is_over(@board) || tie(@board)
       get_human_spot
       if !game_is_over(@board) && !tie(@board)
@@ -19,28 +40,42 @@ class Game
     puts "Game over"
   end
 
-  def get_human_spot
+  def determine_order
+    puts "Would you like to go first? Please enter Y or N \n"
+    answer = gets.upcase.chomp
+      if answer == "Y"
+        human_first
+      elsif answer == "N"
+        puts "Your loss! Muahaha! \n"
+        play_game
+      else
+        puts "I don't understand that. \n"
+        determine_order #make so it breaks eventually?
+      end
+  end
+
+  def get_human_spot #why are you not displayingggggg
     spot = nil
     until spot
       spot = gets.chomp.to_i
-      if @board[spot] != "X" && @board[spot] != "O"
-        @board[spot] = @hum
+      if @board[spot] != "#{@human}" && @board[spot] != "O"
+        @board[spot] = "#{@human}"
       else
         spot = nil
       end
     end
   end
 
-  def eval_board
+  def eval_board #announce if is a tie or not
     spot = nil
     until spot
       if @board[4] == "4"
         spot = 4
-        @board[spot] = @com
+        @board[spot] = @computer
       else
-        spot = get_best_move(@board, @com)
-        if @board[spot] != "X" && @board[spot] != "O"
-          @board[spot] = @com
+        spot = get_best_move(@board, @computer)
+        if @board[spot] != "#{@human}" && @board[spot] != "O"
+          @board[spot] = @computer
         else
           spot = nil
         end
@@ -52,18 +87,18 @@ class Game
     available_spaces = []
     best_move = nil
     board.each do |s|
-      if s != "X" && s != "O"
+      if s != "#{@human}" && s != "O"
         available_spaces << s
       end
     end
     available_spaces.each do |as|
-      board[as.to_i] = @com
+      board[as.to_i] = @computer
       if game_is_over(board)
         best_move = as.to_i
         board[as.to_i] = as
         return best_move
       else
-        board[as.to_i] = @hum
+        board[as.to_i] = @human
         if game_is_over(board)
           best_move = as.to_i
           board[as.to_i] = as
@@ -81,7 +116,7 @@ class Game
     end
   end
 
-  def game_is_over(b)
+  def game_is_over(b) #i don't think it even displays a message? should offer to play again.
 
     [b[0], b[1], b[2]].uniq.length == 1 ||
     [b[3], b[4], b[5]].uniq.length == 1 ||
@@ -94,9 +129,8 @@ class Game
   end
 
   def tie(b)
-    b.all? { |s| s == "X" || s == "O" }
+    b.all? { |s| s == "#{@human}" || s == "O" }
   end
-
 end
 
 game = Game.new
